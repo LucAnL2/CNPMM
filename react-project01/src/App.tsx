60621f689ec0b630225c4ed8e79b071498bfbdbc
“LucAnL2” <huynhthaitoan.12c1htk@gmail.com>
2025-08-18 09:45:37 +0700

    first commit

---
+++ react-project01/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Huỳnh Thái Toàn</h1>
+          <p>MSSV:22110436</p>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
